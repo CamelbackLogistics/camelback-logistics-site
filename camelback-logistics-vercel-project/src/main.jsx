81779bc4daf40e52740c9b0d0e34b0b77ae75a81
CamelbackLogistics <info@camelbacklogistics.com>
2026-05-27 14:36:01 -0400

    Update main.jsx

--- camelback-logistics-vercel-project/src/main.jsx
+++ camelback-logistics-vercel-project/src/main.jsx
@@ -15,7 +15,7 @@ const heroImages = [
   },
   {
     label: "Rail",
-    src: "https://images.unsplash.com/photo-1474487548417-781cb71495f3?auto=format&fit=crop&w=2400&q=80"
+    src: "https://images.unsplash.com/photo-1537426032279-4bb4d0f683b1?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     label: "Warehouse",
