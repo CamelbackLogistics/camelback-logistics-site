511310b32756753bfdcb0a02054638e224880ff9
CamelbackLogistics <info@camelbacklogistics.com>
2026-05-27 14:37:14 -0400

    Update main.jsx

--- camelback-logistics-vercel-project/src/main.jsx
+++ camelback-logistics-vercel-project/src/main.jsx
@@ -19,7 +19,7 @@ const heroImages = [
   },
   {
     label: "Warehouse",
-    src: "https://images.unsplash.com/photo-1586528116311-ad8dd3c8310d?auto=format&fit=crop&w=2400&q=80"
+    src: "https://images.unsplash.com/photo-1777026321659-64941fb943dd?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   }
 ];
 
