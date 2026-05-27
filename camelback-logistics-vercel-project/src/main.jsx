16d6248815489cdc5a40575081ee64fa1c487b51
CamelbackLogistics <info@camelbacklogistics.com>
2026-05-27 14:30:21 -0400

    Update main.jsx

--- camelback-logistics-vercel-project/src/main.jsx
+++ camelback-logistics-vercel-project/src/main.jsx
@@ -11,7 +11,7 @@ const heroImages = [
   },
   {
     label: "Truck",
-    src: "https://unsplash.com/photos/blue-truck-on-road-near-bare-trees-during-daytime-jOi8CLM2aaI?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink"
+    src: "https://images.unsplash.com/photo-1594010465298-8522f3325429?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     label: "Rail",
